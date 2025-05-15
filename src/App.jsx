840c7828247143760548e258b02d185b64fa92eb
Sonissapkota <sonishsapkota47@gmail.com>
2025-05-15 09:23:45 +0545

    Created the gridlines background for the site

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import './App.css'
-
 function App() {
-
+  return(
+  <>
+    <div>
+      <h2>My Portfolio</h2>
+      <p>I am a backend developer.</p>
+    </div>
+  </>
+  )
 }
 
 export default App
